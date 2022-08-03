55962c98585720643d422de2dbe17ccc632d91de
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-08-03 23:05:28 +0500

    Adding bootstrap and making navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Pizza world</h1>
+      <Navbar/>
     </div>
   );
 }
