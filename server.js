cc46be4536228d46a043c90d063c2d9b9f264c45
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-08-05 23:38:08 +0500

    Making pizzaModel and getallpizzas API

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 // Making server by first importing express in express variable.
 const express = require("express");
-const { config } = require("process");
+
+// Importing Pizza Model in Pizza variable
+const Pizza = require('./models/pizzaModel')
 
 const db = require('./db.js')
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+
+// Making API for getting all pizza
+app.get("/getallpizzas",(req,res)=>{
+
+    // Finding all data from Pizza
+    Pizza.find({},(err,docs)=>{
+        if (err) {
+            console.log(error)
+        } else {
+            res.send(docs)
+        }
+    })
+
+})
 
 const port = process.env.PORT || 5000;
 
