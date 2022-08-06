7d29fc1871ced5658f365a13ad2bca97a177c079
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-08-06 23:01:18 +0500

    Rendering pizzas through state in homescreen

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,34 +1,39 @@
 // Import section
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Pizzas from '../components/Pizzas'
 import pizzas from '../pizzasdata'
-import {useDispatch,useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getAllPizzas } from '../actions/pizzaActions'
 
 const Homescreen = () => {
   const dispatch = useDispatch()
+  const pizzasstate = useSelector(state => state.getAllPizzasReducer)
+  const { pizzas, loading, error } = pizzasstate
   useEffect(() => {
     // Calling the action name
     dispatch(getAllPizzas())
   }, [])
-  
+
   return (
     <>
-    {/* Making a row and rendering pizza data in col-md-4 */}
-    <div className="row">
-      {/* Maping the pizza data */}
-        {pizzas.map(pizza=>{
-            return <>
-                <div className="col-md-4">
-                    <div>
-                      {/* Giving mapping data as a props */}
-                    <Pizzas pizza={pizza} />
+      {/* Making a row and rendering pizza data in col-md-4 */}
+      <div className="row">
+        {loading ? (<h1>Loading</h1>) : error ? (<h1>Something went wrong</h1>) : (
 
-                    </div>
+          pizzas.map(pizza => {
+            return <>
+              <div className="col-md-4" key={pizza._id}>
+                <div >
+                  {/* Giving mapping data as a props */}
+                  <Pizzas pizza={pizza} />
+
                 </div>
+              </div>
             </>
-        })}
-    </div>
+          })
+        )}
+
+      </div>
     </>
   )
 }
