4786a69cf2edb6d1a1924f78e6a704983fa70322
Shoaib Jamil <shoaibjamil43@gmail.com>
2022-08-06 21:02:58 +0500

    Making a redux action and resucer for getAllPizzas

---
+++ client/src/actions/pizzaActions.js
@@ -0,0 +1,14 @@
+// Import section
+import axios from 'axios';
+
+export const getAllPizzas = ()=>dispatch=>{
+    dispatch({type:'GET_PIZZAS_REQUEST'})
+
+    try {
+        const response = axios.get('/api/pizzas/getallpizzas')
+        console.log(response)
+        dispatch({type:'GET_PIZZAS_SUCCESS',payload : response.data})
+    } catch (error) {
+        dispatch({type:'GET_PIZZAS_FAILED',payload:error})
+    }
+}
